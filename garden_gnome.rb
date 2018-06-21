d4d14777e18ec06f511ee13ae6628537e074519f
ZoraThomas <zt081203@wgcloud.org>
2018-06-21 21:00:25 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,7 @@
 class GardenGnome
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :gluten_allergy, :personality
   
-  def initialize(name, age)
+  def initialize(name, age, gluten_allergy, personality)
     @name
     @age
   end
